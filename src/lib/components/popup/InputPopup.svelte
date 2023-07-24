8bba70f84f1b534feeaca6345a92f4f9f940e32d
Bulat Latypov <b.latypov@innopolis.university>
2023-07-24 15:51:40 +0300

    Changed Input Popup

--- src/lib/components/popup/InputPopup.svelte
+++ src/lib/components/popup/InputPopup.svelte
@@ -1,31 +1,41 @@
 <script lang="ts">
-	import Button from '$lib/components/base/Button.svelte';
+	import Button from "$lib/components/base/Button.svelte";
 	import { fade } from 'svelte/transition';
-	import { inputOverlayText } from '$lib/stores';
+	import {inputOverlayText, outputOverlayText} from "$lib/stores";
+	import InputTemplate from "$lib/components/base/inputs/InputTemplate.svelte";
 	export let popupText: string;
 	export let firstPlayer: string;
 	export let secondPlayer: string;
-	const buttonPressed = () => {
+	const buttonPressed = (e: Event) => {
+		e.preventDefault();
+		let data = new FormData(e.target as HTMLFormElement);
+		$outputOverlayText = [Number(data.get('firstScore')), Number(data.get('secondScore'))];
 		$inputOverlayText = null;
 	};
 </script>
 
 <form class="fullscreen-overlay" transition:fade on:submit={buttonPressed}>
-	<button class="overlay-background" type="submit" />
+	<button class="overlay-background" type="button"></button>
 	<div class="overlay-content">
 		<div class="overlay-head">
 			<div class="circle">!</div>
 		</div>
 		<p class="overlay-text">{popupText}</p>
 		<div class="inputScore">
-			<label class="first-player">
+			<div class="player-block">
 				<p>{firstPlayer}</p>
-				<input type="number" id="first-score" value="0" />
-			</label>
-			<label class="second-player">
+				<div class="input">
+					<InputTemplate type={"number"} name={"firstScore"} defaultNumValue={0} placeholder={"First Score"}></InputTemplate>
+				</div>
+
+			</div>
+			<div class="player-block">
 				<p>{secondPlayer}</p>
-				<input type="number" id="second-score" value="0" />
-			</label>
+				<div class="input">
+					<InputTemplate type={"number"} name={"secondScore"} defaultNumValue={0} placeholder={"Second Score"}></InputTemplate>
+				</div>
+			</div>
+
 		</div>
 		<div class="overlay-button-block">
 			<div class="overlay-button">
@@ -62,7 +72,7 @@
 		position: relative;
 		background: var(--main-color);
 		z-index: 1;
-		height: 15rem;
+		height: 20rem;
 		width: 25rem;
 		text-align: center;
 		border-radius: 30px;
@@ -72,11 +82,11 @@
 	}
 	.overlay-head {
 		display: flex;
-		height: 30%;
+		height: 25%;
 		background-color: var(--secondary-color);
 		border-top-left-radius: 30px;
 		border-top-right-radius: 30px;
-		align-items: center;
+		align-items:  center;
 		justify-content: center;
 	}
 	.circle {
@@ -92,14 +102,13 @@
 		font-weight: var(--fontweight-1);
 	}
 	.overlay-text {
-		margin: 1rem 1rem 1rem 0;
-		font-size: var(--fontsize-medium1);
-		font-weight: var(--fontweight-2);
+		font-size: var(--fontsize-large);
+		margin: 1rem auto;
 	}
 	.overlay-button-block {
 		position: absolute;
 		display: flex;
-		bottom: 10%;
+		bottom: 5%;
 		height: 15%;
 		width: 100%;
 		justify-content: center;
@@ -107,29 +116,47 @@
 	.overlay-button {
 		width: 20%;
 	}
+	.player-block {
+		width: 40%;
+	}
+	.player-block p {
+		width: 100%;
+		height: 2rem;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
 	.inputScore {
+		margin-top: 1.5rem;
 		width: 100%;
 		display: flex;
 		justify-content: space-around;
 	}
-	.inputScore label {
-		width: 40%;
+	.input {
+		width: 80%;
+		margin: 0.5rem auto;
 	}
-	.inputScore label input {
-		width: 50%;
-	}
-	.inputScore label p {
-		height: 2rem;
-	}
-	@media (max-width: 800px) {
+	@media(max-width: 800px) {
 		.overlay-content {
-			height: 45%;
+			height: 50%;
 			width: 60%;
 		}
+		.player-block {
+			width: 40%;
+		}
 	}
-	@media (max-width: 480px) {
+	@media(max-width: 480px) {
 		.overlay-content {
-			height: 20rem;
+			padding: 0;
+			height: 30rem;
+			width: 70%;
+		}
+		.overlay-head {
+			height: 20%;
+		}
+		.overlay-text {
+			font-size: var(--fontsize-large);
+			margin-top: 2rem;
 		}
 		.overlay-button-block {
 			bottom: 5%;
@@ -139,10 +166,16 @@
 		}
 		.inputScore {
 			display: block;
+			margin: 2rem auto;
 		}
-		.inputScore label p {
-			height: 1rem;
-			margin-bottom: 0.4rem;
+		.player-block {
+			width: 80%;
+			margin: 1rem auto;
+			text-align: center;
+		}
+		.input {
+			margin: 0 auto;
+			width: 70%;
 		}
 	}
 </style>
